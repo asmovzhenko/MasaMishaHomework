5c3faded9642a2dc0aa91bab39b38a510544f735
asmovzhenko <asmovzhenko@gmail.com>
2019-09-28 17:36:15 +0300

    first homework with events and google search

---
+++ index.js
@@ -0,0 +1,47 @@
+let input = document.getElementById('input');
+
+
+let submit = document.getElementById('submit');
+let list = document.getElementById('list');
+
+function GoodColor(color) {
+    var color2 = "";
+    var result = true;
+    var e = document.getElementById('mydiv');
+    e.style.borderColor = "";
+    e.style.borderColor = color;
+    color2 = e.style.borderColor;
+    if (color2.length == 0) {
+        result = false;
+    }
+    e.style.borderColor = "";
+    return result;
+}
+
+
+function submitText() {
+    let value = input.value;
+    if (GoodColor(value) == true) {
+
+        let node = document.createElement("li");
+        node.innerText = value;
+        node.style.color = value;
+        list.appendChild(node);
+    } else {
+        console.log('error');
+    }
+}
+
+function googleOnClick() {
+    let query = input.value;
+    if (GoodColor(query) == true) {
+        window.open(' https://www.google.com/search?q=' + query);
+    } else {
+        console.log('error');
+    }
+}
+
+let googleButton = document.getElementById('google');
+googleButton.onclick = googleOnClick;
+
+submit.onclick = submitText;
